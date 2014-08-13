ff779cdfad9faacb6831b5b22ee6a27130176473
Tobi Weinstock <tv.weinstock@gmail.com>
2014-08-13 16:31:31 -0400

    steps 1-7 added for grocery list

--- exercise6.rb
+++ exercise6.rb
@@ -1,3 +1,43 @@
 grocery_list = ["carrots", "tissues", "apples", "salmon"]
 
-def
+#step one
+grocery_list.each do |item|
+	puts '*' + item
+end
+
+#step two
+grocery_list << "rice"
+
+grocery_list.each do |item|
+	puts '*' + item
+end
+
+#method to call for up-to-date grocery list
+def updated_list(list)
+	list.each do |item|
+	puts '*' + item
+	end
+end
+
+updated_list(grocery_list)
+
+#step three.... to check number of items
+puts grocery_list.length
+
+#step four... looking for bananas
+if grocery_list.include?("bananas")
+	puts "You need to pick up bananas."
+else 
+	puts "No need to pick up bananas today."
+end
+
+#step five...displaying second item on list
+puts grocery_list[1]
+
+#step six...alphabetical sorting
+updated_list(grocery_list.sort!)
+
+#step seven...deleted salmon and displayed updated list
+grocery_list.delete_at(3)
+updated_list(grocery_list)
+
