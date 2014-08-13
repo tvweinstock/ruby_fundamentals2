ec8864e188d73fbd08bcdd8057ca0b3d52d8e58f
Tobi Weinstock <tv.weinstock@gmail.com>
2014-08-13 14:57:56 -0400

    second stab at excercise5

--- exercise5.rb
+++ exercise5.rb
@@ -1,8 +1,8 @@
-puts "Hey, what's the temperature in Fahrenheit?"
-temp_fahr = gets.chomp.to_i
-
-def convert_temp(temp_fahr)
-	return (temp_fahr - 32) * 5/9
+def convert_temp(temp_f)
+	return (temp_f - 32) * 5/9
 end
 
-puts "And the temperature in celsius is #{convert_temp(temp_fahr)}."
+puts "Hey, what's the temperature in Fahrenheit?"
+temp_fahr = gets.chomp.to_i 
+temp_cel = convert_temp(temp_fahr)
+puts "And the temperature in celsius is #{temp_cel}."
